f6c4c71ae1e13bbc8edf184696c81997e630fecc
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-04-15 21:58:20 -0500

    Initial webpack server config

---
+++ source/server.js
@@ -0,0 +1,16 @@
+import http from 'http';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+function requestHandler(req, res) {
+  const html = renderToString(
+    React.DOM.h1(null, 'init')
+  );
+
+  res.write(html);
+  res.end();
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(3000)
